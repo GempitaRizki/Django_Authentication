eae87debc5289b237d07d55abeda98e63c461ca8
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-06-24 13:38:20 +0700

    create user into panel django

--- app1/views.py
+++ app1/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def HomePage(request):
-    pass
+    return render(request, 'home.html')
 
 def SignupPage(request):
-    return render (request, 'signup.html')
+    if request.method == 'POST':
+        username =request.POST.get('username')
+        email=request.POST.get('email')
+        pas1=request.POST.get('password1')
+        pas2=request.POST.get('password2')
+        my_user=User.objects.create_user(username,email,pas1)
+        my_user.save()
+        return HttpResponse("Akun berhasil dibuat")
+        print(username,email,pas1,pas2)
+
+    return render(request, 'signup.html')
 
 def LoginPage(request):
-    pass
+    return render(request, 'login.html')
 
